0e89dcc930b64dde951db7a0dc073a3999403ce6
mobin afshari <mma137813781378@gmail.com>
2025-01-10 22:46:34 +0330

    fix: testing vite for chunks splitting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react-vendor'; // Separate React-related code
+            }
+            return 'vendor'; // General vendor chunk
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': '/src',
