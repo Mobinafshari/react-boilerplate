7fcfa61e6872f2c3576d1ce25cf67347dbd4f18f
mobin afshari <mma137813781378@gmail.com>
2025-01-10 22:22:11 +0330

    fix: main file fixed

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,14 @@ import App from './App';
 import './styles/main.scss';
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((registration) => {
-        console.log(
-          'Service Worker registered with scope:',
-          registration.scope
-        );
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered:', registration);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
