ec93119da6b3daac058bc444e5881727536295b5
mobin afshari <mma137813781378@gmail.com>
2025-01-25 21:02:12 +0330

    fix:eslint fixed for test folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,39 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import testingLibrary from 'eslint-plugin-testing-library';
+import { fixupPluginRules } from '@eslint/compat';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['node_modules', 'dist', 'cypress' , "test" ] },
-  { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}' ,] },
+  { ignores: ['node_modules', 'dist', 'cypress'] },
+  { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}', 'test/**/test.{ts , tsx}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        it: 'readonly',
+        jest: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        indexedDB: 'readonly',
+        describe: 'readonly',
+      },
+    },
+  },
+  {
+    plugins: {
+      'testing-library': fixupPluginRules({
+        rules: testingLibrary.rules,
+      }),
+    },
+  },
   {
     settings: {
       react: {
