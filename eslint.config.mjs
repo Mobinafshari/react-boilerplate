fea46f331b796eb5c43a3039ea65736f3e4a1ea0
mobin afshari <mma137813781378@gmail.com>
2025-01-25 20:47:12 +0330

    fix: tsconfig file fixed and errors gone

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import testingLibrary from "eslint-plugin-testing-library";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['node_modules', 'dist', 'cypress' ] },
-  { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}' , "test/**/test.{ts , tsx}"] },
+  { ignores: ['node_modules', 'dist', 'cypress' , "test" ] },
+  { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}' ,] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  testingLibrary.configs.react,
   {
     settings: {
       react: {
